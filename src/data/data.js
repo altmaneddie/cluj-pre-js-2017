0db331a6d190feb003d13b3ff173f6c1cc9a3f33
altmaneddie <altman.eduard@gmail.com>
2017-08-25 17:49:37 +0300

    fieldset almost done

--- src/data/data.js
+++ src/data/data.js
@@ -1,33 +1,32 @@
-function getTechinicalData() {
-    return
-    {
-        title: "Technical Level",
-            headings: ['Trainee', 'Junior', 'Middle', 'Senior'],
-                columnData: [
-                    { inputLevels: ["Trainee"] },
-                    { inputLevels: ["Junior1", "Junior2", "Junior3"] },
-                    { inputLevels: ["Middle1", "Middle2", "Middle3"] },
-                    { inputLevels: ["Senior1", "Senior2", "Senior3"] }
-                ]
-    }
+function getTechnicalData() {
+    return {
+        title: 'Technical Level',
+        headings: ['Trainee', 'Junior', 'Middle', 'Senior'],
+        columnData: [
+            { inputLevels: ['Trainee'] },
+            { inputLevels: ['Junior1', 'Junior2', 'Junior3'] },
+            { inputLevels: ['Middle1', 'Middle2', 'Middle3'] },
+            { inputLevels: ['Senior1', 'Senior2', 'Senior3'] },
+        ],
+    };
 }
 
 function getTextareaData() {
     return {
         textarea:
         [{
-            label: "Should the candidate be hired?",
-            placeholder: "The type of project that is suitable for the candidate &#13;&#10;Is guidance required for the candidate"
+            label: 'Should the candidate be hired?',
+            placeholder: 'The type of project that is suitable for the candidate &#13;&#10;Is guidance required for the candidate',
         },
         {
-            label: "General Impression",
-            placeholder: "*required"
+            label: 'General Impression',
+            placeholder: '*required',
         },
         {
-            label: "Workflow, Leadership &amp; Soft Skills",
-            placeholder: "Describe the environment in which the candidate works.&#13;&#10;Describe any guidance or management experience"
-        }]
-    }
+            label: 'Workflow, Leadership &amp; Soft Skills',
+            placeholder: 'Describe the environment in which the candidate works.&#13;&#10;Describe any guidance or management experience',
+        }],
+    };
 }
 
 function getFieldsetData() {
@@ -69,7 +68,7 @@ function getFieldsetData() {
                         options: ["0", "1", "2", "3"]
                     }
                 ],
-            {
+                {
                 legend: "HTTP",
                 ul: [
                     {
--- src/views/new-evaluation/new-evaluation.js
+++ src/views/new-evaluation/new-evaluation.js
@@ -1,18 +1,15 @@
-function NewEvaluationForm(options){
-    const technicalObj = getTechinicalData()   ;
+function NewEvaluationForm(options = {}) {
+    const technicalObj = getTechnicalData();
     const textareaObj = getTextareaData();
-
-
     return `
-    ${CandidateDetailsform()}
+    ${CandidateDetailsForm()}
     ${TechnicalLevelPicker(technicalObj)}
     ${Textarea(textareaObj)}
-    
     `
 }
 
 
-function CandidateDetailsform(options={}){
+function CandidateDetailsForm(options = {}) {
     return `
         <form class="main-form" method="POST" action="newEval.php">
         <div class="data-form">
@@ -23,13 +20,11 @@ function CandidateDetailsform(options={}){
      `
 }
 
-
-
-function TechinicalLevelrow1Creator(options) {
-    return `<th>${options.headings.join(`</th><th>`)}</th>`
+function TechnicalLevelRow1Creator(options) {
+    return `<th>${options.headings.join('</th><th>')}</th>`
 };
 
-function TechinicalLevelrow2Creator(options) {
+function TechnicalLevelRow2Creator(options) {
 
     let newArr = []
 
@@ -42,34 +37,70 @@ function TechinicalLevelrow2Creator(options) {
             `}).join('')}
      </td>`)
     })
-    return newArr.join("");
+    return newArr.join('');
 }
 
-function Textarea(options={}){
+function TechnicalLevelPicker(options = {}) {
+    return `
+    <table class="table">
+    <tr class="row1">
+        ${TechnicalLevelRow1Creator(options)}
+    </tr>
+    <tr class="row2">
+        ${TechnicalLevelRow2Creator(options)}
+    </tr>
+    </table>
+    `
+}
 
-        
+function TextareaCreator(options){
+   return options.textarea.map(function(k){
+      return `<h3>${k.label}</h3>
+        <textarea class="textA" rows="5" cols="80" placeholder="${k.placeholder}"></textarea>
+        `}).join('')
+    }
 
+function Textarea(options= {}) {
+    return `
+    <div class="impression">
+    ${TextareaCreator(options)}
+    </div>
+    `
+}
+
+function FieldsetUlCreator(options){
+
+    options.fieldset.map(k) =>
+    `
+    <legend class="eval-forms-title">${k.legend}</legend>
+    <ul class="eval-forms">
+    ${FieldsetLiCreator}
+    </ul>
+    `
 }
 
 
+function FieldsetLiCreator(options) {
+    options.ul.map((m) =>
+    `
+    <li>${m.label}</li>
+    <li>
+        <select name="${m.label} To use string.replace">
+        ${FieldsetOptionsCreator(options)}
+        </select>
+`)
+}
+
+function FieldsetOptionsCreator(options){
+      return  options.options.map((n) =>
+            `
+            <option  value="${n}">${n}</option>
+            `
+        ).join('')
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.onload = function(){
+window.onload = function () {
     const appEl = document.querySelector("#app");
     appEl.innerHTML = NewEvaluationForm();
 }
