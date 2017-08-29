ea23127ea0dcdc5d87d454e8aaf21d35db3d86a0
altmaneddie <altman.eduard@gmail.com>
2017-08-29 22:20:48 +0300

    finalizing the events

--- index.js
+++ index.js
@@ -14,27 +14,27 @@ window.addEventListener("load", function() {
 
 // display pages functions
 
-function switchToLogin(event){
-    theBody.innerHTML ='';
-    theBody.innerHTML = LoginPage();
-    event.preventDefault();
-}
+// function switchToLogin(event){
+//     theBody.innerHTML ='';
+//     theBody.innerHTML = LoginPage();
+//     event.preventDefault();
+// }
 
-function switchToEval(event){
-    theBody.innerHTML ='';
-    theBody.innerHTML = EvaluationsPage();
-    event.preventDefault();
-}
+// function switchToEval(event){
+//     theBody.innerHTML ='';
+//     theBody.innerHTML = EvaluationsPage();
+//     event.preventDefault();
+// }
 
-function switchToNewEval(event){
-    theBody.innerHTML ='';
-    theBody.innerHTML = NewEvaluationForm();
-    event.preventDefault();
-}
+// function switchToNewEval(event){
+//     theBody.innerHTML ='';
+//     theBody.innerHTML = NewEvaluationForm();
+//     event.preventDefault();
+// }
 
-// event listeners
+// // event listeners
 
-evalBtn.addEventListener("click",switchToEval(event));
-newEvalBtn.addEventListener("click",switchToNewEval(event));
-submitBtn.addEventListener("click",switchToEval(event));
-logoutBtn.addEventListener("click",switchToLogin(event));
+// evalBtn.addEventListener("click",switchToEval(event));
+// newEvalBtn.addEventListener("click",switchToNewEval(event));
+// submitBtn.addEventListener("click",switchToEval(event));
+// logoutBtn.addEventListener("click",switchToLogin(event));
--- src/events/evaluation.js
+++ src/events/evaluation.js
@@ -1,8 +1,15 @@
 function addEventstoEval(){
     const newEvalBtn = document.getElementById('newEvaluationBtn');
+    const logoutBtn = document.getElementById('logoutBtn');
     newEvalBtn.addEventListener("click",function(e){
         event.preventDefault();
         theBody.innerHTML ='';
         theBody.innerHTML = NewEvaluationForm();
+        addEventstoNewEval();
+    })
+    logoutBtn.addEventListener("click",function(e){
+        event.preventDefault();
+        theBody.innerHTML = LoginPage();
+        addEventstoLogin();
     })
 }
